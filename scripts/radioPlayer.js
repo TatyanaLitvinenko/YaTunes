71c87e7a1d40681bc57d4d3c67207cada7e4ebdf
Litvinenko Tatyana <dead_jane@icloud.com>
2020-07-29 18:20:11 +0500

    lesson 3 and volume bar

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,3 +1,88 @@
 export const radioPlayerInit = () => {
-    console.log('Radio Init');
+    const radio = document.querySelector('.radio');
+    const radioNavigation = document.querySelector('.radio-navigation');
+    const radioCoverImg = document.querySelector('.radio-cover__img');
+    const radioItem = document.querySelectorAll('.radio-item');
+    const radioHeaderBig = document.querySelector('.radio-header__big');
+    const radioStop = document.querySelector('.radio-stop');
+    const radioVolume = document.querySelector('.radio-volume');
+    const radioMute = document.querySelector('.radio-mute');
+
+    const audio = new Audio();
+    audio.type = 'audio/aac';
+
+    radioStop.disabled = true;
+
+    const toggleIconPlay = () => {
+        if (audio.paused) {
+            radio.classList.remove('play');
+            radioStop.classList.add('fa-play');
+            radioStop.classList.remove('fa-stop');
+        } else {
+            radio.classList.add('play');
+            radioStop.classList.add('fa-stop');
+            radioStop.classList.remove('fa-play');
+        }
+    }
+
+    const toggleVolumeIcon = () => {
+        if (audio.volume === 0) {
+            radioMute.classList.remove('fa-volume-up');
+            radioMute.classList.add('fa-volume-off');
+        } else {
+            radioMute.classList.add('fa-volume-up');
+            radioMute.classList.remove('fa-volume-off');
+        }
+    }
+
+    const toggleMute = () => {
+        if (audio.volume != 0) {
+            audio.volume = 0;
+        } else {
+            audio.volume = radioVolume.value / 100;
+        }
+    }
+
+    const selectItem = elem => {
+        radioItem.forEach(item => item.classList.remove('select'));
+        elem.classList.add('select');
+    }
+
+    radioNavigation.addEventListener('change', event => {
+        radioStop.disabled = false;
+        const target = event.target;
+
+        const parent = target.closest('.radio-item');
+        selectItem(parent);
+
+        const title = parent.querySelector('.radio-name').textContent;
+        radioHeaderBig.textContent = title;
+        
+        const imgUrl = parent.querySelector('.radio-img').src;
+        radioCoverImg.src = imgUrl;
+
+        audio.src = target.dataset.radioStantion;
+        audio.play();
+        toggleIconPlay();
+    });
+
+    radioStop.addEventListener('click', () => {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+        toggleIconPlay();
+    });
+
+    radioVolume.addEventListener('input', () => {
+        audio.volume = radioVolume.value / 100;
+    });
+
+    
+    radioMute.addEventListener('click', toggleMute);
+    radioMute.addEventListener('click', toggleVolumeIcon);
+    
+    
+    radioVolume.value = audio.volume * 100;
 };
